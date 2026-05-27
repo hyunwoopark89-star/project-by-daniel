59bf0ed6074b4d54437205162faac1308130a188
Hyunwoo Park <hyunwoopark89@gmail.com>
2026-05-26 22:24:36 -0700

    4 control flow

---
+++ 4 control flow.py
@@ -0,0 +1,27 @@
+x=10
+y=5
+if x>y:
+    print('x is greater than y')
+else:
+    print('x is not greater than y')
+
+age=70
+if age<18:
+    print('You are a minor')
+elif age>=18 and age<65:
+    print('You are an adult')
+else:
+    print('You are a senior citizen')
+
+for i in range(1,12):
+    print(i)
+
+count = 1
+while count <=7:
+    print(count)
+    count += 1
+
+decrement = 10
+while decrement > 0:
+    print(decrement)
+    decrement -= 2
